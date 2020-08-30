0ae06b6b240c9a71eabc6dfa760a30fff7d9d3f3
joedillonuk <joe.dillon.uk@gmail.com>
2020-08-31 00:51:00 +0100

    Implemented session handling.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,22 +11,23 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes'
+import { withAuthentication } from '../Session';
 
 const App = () => (
-  <Router>
-  <div>
-    <Navigation />
+      <Router>
+      <div>
+      <Navigation />
 
-    <hr />
-    <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-          <Route path={ROUTES.HOME} component={HomePage} />
-          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-          <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
-);
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+      </div>
+      </Router>
+    );
 
-export default App;
+export default withAuthentication(App);
